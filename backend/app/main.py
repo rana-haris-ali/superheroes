a73091d6ff520f0ae8f6c97bb6468a0926257c73
Rana Haris Ali <ranaharisali42@yahoo.com>
2024-09-08 03:01:23 +0200

    style(backend): fix formatting

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,6 @@ from app.core.config import settings
 app = FastAPI(title=settings.project_name, version=settings.project_version)
 
 
-@app.get('/')
+@app.get("/")
 def hello_world():
-	return 'Hello World'
+    return "Hello World"
--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,5 @@
 """export models from a single source
    to help alembic detect them"""
+
 from app.core.database import Base
 from .user import User
