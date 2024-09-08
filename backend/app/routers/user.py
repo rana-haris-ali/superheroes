5a9117101ba12a684d4d49cb6bdcbf9a0afe72cc
Rana Haris Ali <ranaharisali42@yahoo.com>
2024-09-08 22:22:09 +0200

    refactor(backend): use starlette's status dictionary to assign HTTP status codes, rename UserSchema to UserPublicSchema

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -1,13 +1,16 @@
 from fastapi import APIRouter, HTTPException
+from starlette import status
 
 from app.dependencies.db_dependency import DBSessionDep
-from app.schemas.user import UserSchema, UserCreate
+from app.schemas.user import UserPublicSchema, UserCreate
 from app.services.user import create_user, get_user_by_email
 
 user_router = APIRouter(prefix="/users", tags=["User"])
 
 
-@user_router.post("/", response_model=UserSchema)
+@user_router.post(
+    "/", status_code=status.HTTP_201_CREATED, response_model=UserPublicSchema
+)
 def user_signup(user: UserCreate, db: DBSessionDep):
     email_already_registered = get_user_by_email(db, email=user.email)
     if email_already_registered:
