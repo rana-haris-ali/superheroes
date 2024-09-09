01b0c06e064ed0982bf212036485ae09ee523c0b
Rana Haris Ali <ranaharisali42@yahoo.com>
2024-09-09 03:08:48 +0200

    feat(superheros): implement "get superhero by id" API

--- backend/app/services/superhero.py
+++ backend/app/services/superhero.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 from app.models import Superhero
 from app.schemas.pagination import PagedResponseSchema, PageParamsSchema
-from app.schemas.superhero import SuperheroBaseSchema
+from app.schemas.superhero import SuperheroBaseSchema, SuperheroDetailsSchema
 from app.utils.pagination import paginate
 
 
@@ -10,3 +10,7 @@ def get_superheros(
 ) -> PagedResponseSchema[SuperheroBaseSchema]:
     query = db.query(Superhero)
     return paginate(page_params, query, SuperheroBaseSchema)
+
+
+def get_superhero_by_id(superhero_id: int, db: Session) -> Superhero | None:
+    return db.query(Superhero).filter(Superhero.id == superhero_id).first()
