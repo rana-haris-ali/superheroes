2797b0dd6a3f6b8019a14c97156f8e8aba67496e
Rana Haris Ali <ranaharisali42@yahoo.com>
2024-09-10 04:58:28 +0200

    chore(backend): add CORS config

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 from app.routers.superhero import superhero_router
 from app.routers.user import user_router
@@ -6,6 +7,15 @@ from app.routers.auth import auth_router
 
 app = FastAPI(title=settings.project_name, version=settings.project_version)
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def hello_world():
