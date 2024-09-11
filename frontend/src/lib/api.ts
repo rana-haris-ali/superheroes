e83df1ed7c99b5b6eac19c4aaba38eb071a96898
Rana Haris Ali <ranaharisali42@yahoo.com>
2024-09-11 21:33:59 +0200

    chore(types): create Team types

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -2,6 +2,7 @@ import { Paginated, PaginationParams } from '@/types/pagination';
 import apiClient from './axios'; // Import the Axios instance
 import { SuperheroBaseType, SuperheroDetailsType } from '@/types/superhero'
 import { UserSignupParams } from '@/types/user';
+import { CreateTeamType, TeamBaseType } from '@/types/team';
 
 // User signup
 export const userSignup = async (signupParams: UserSignupParams) => {
@@ -20,9 +21,28 @@ export const fetchSuperheroById = async ({ id }: { id: number }): Promise<Superh
 };
 
 // Fetch superheroes
-export const fetchSuperheroes = async (paginationParams: PaginationParams): Promise<Paginated<SuperheroBaseType>> => {
+export const fetchSuperheroes = async (
+	paginationParams: PaginationParams,
+	searchText: string | null = null
+): Promise<Paginated<SuperheroBaseType>> => {
 	const response = await apiClient.get('/superheroes', {
-		params: paginationParams
+		params: {
+			...paginationParams,
+			...(searchText && {
+				search_query: searchText
+			})
+		}
+	});
+	return response.data;
+};
+
+// create team
+export const createTeam = async (
+	teamParams: CreateTeamType
+): Promise<TeamBaseType> => {
+	const response = await apiClient.post('/teams', {
+		name: teamParams.name,
+		team_members: teamParams.teamMembers
 	});
 	return response.data;
 };
