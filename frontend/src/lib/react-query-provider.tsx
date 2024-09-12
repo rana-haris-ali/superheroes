76d32334bf5823e6e202eeca30fd314037de3a7c
Rana Haris Ali <ranaharisali42@yahoo.com>
2024-09-12 16:41:21 +0200

    misc: comment unused variable

--- frontend/src/lib/react-query-provider.tsx
+++ frontend/src/lib/react-query-provider.tsx
@@ -2,7 +2,7 @@
 
 import { ReactNode, useEffect, useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useAuth } from '@/app/context/auth-context';
 
 export default function ReactQueryProvider({
